9247c1a3fef69669f0cb5e51c87213009be97a1b
Muhammad Daniyal Khan <mdk968196@gmail.com>
2024-08-22 21:17:06 +0500

    delete functionality added

--- lib/doctors_management/screens/chatlistscreen.dart
+++ lib/doctors_management/screens/chatlistscreen.dart
@@ -70,6 +70,8 @@ class _DoctorChatPatientListScreenState
 
         // Fetch profile picture URL from the 'patients' collection
         String patientId = patientData['patientId'];
+        String appointmentId = doc.id; // Get the appointmentId
+
         DocumentSnapshot patientSnapshot =
             await _firestore.collection('patients').doc(patientId).get();
 
@@ -78,8 +80,9 @@ class _DoctorChatPatientListScreenState
               patientSnapshot.data() as Map<String, dynamic>?;
           String profilePicUrl = patientInfo?['profile_image'] ?? '';
 
-          // Add profilePicUrl to patientData
+          // Add profilePicUrl and appointmentId to patientData
           patientData['profile_image'] = profilePicUrl;
+          patientData['appointmentId'] = appointmentId; // Store appointmentId
           fetchedPatients.add(patientData);
         }
       }
@@ -187,6 +190,8 @@ class _DoctorChatPatientListScreenState
                                                           patient['name'],
                                                       patientId:
                                                           patient['patientId'],
+                                                      appointmentId: patient[
+                                                          'appointmentId'], // Pass appointmentId
                                                     )));
                                       },
                                     )
@@ -208,3 +213,206 @@ class _DoctorChatPatientListScreenState
     );
   }
 }
+
+
+// class DoctorChatPatientListScreen extends StatefulWidget {
+//   @override
+//   _DoctorChatPatientListScreenState createState() =>
+//       _DoctorChatPatientListScreenState();
+// }
+
+// class _DoctorChatPatientListScreenState
+//     extends State<DoctorChatPatientListScreen> {
+//   int _selectedIndex = 0;
+//   late String doctorId;
+
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//     if (index == 0) {
+//       Navigator.push(
+//           context, MaterialPageRoute(builder: (context) => DoctorHomeScreen()));
+//     }
+//     if (index == 1) {
+//       Navigator.push(context,
+//           MaterialPageRoute(builder: (context) => DoctorProfileScreen()));
+//     }
+//     if (index == 2) {
+//       Navigator.push(context,
+//           MaterialPageRoute(builder: (context) => DoctorAvailabilityScreen()));
+//     }
+//     if (index == 3) {
+//       Navigator.push(
+//           context,
+//           MaterialPageRoute(
+//               builder: (context) => DoctorChatPatientListScreen()));
+//     }
+//   }
+
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+//   List<Map<String, dynamic>> patients = [];
+//   bool isLoading = true;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _fetchPatients();
+//   }
+
+//   void _fetchPatients() async {
+//     User? user = _auth.currentUser;
+//     if (user != null) {
+//       QuerySnapshot appointmentsSnapshot = await _firestore
+//           .collection('appointments')
+//           .where('doctorId', isEqualTo: user.uid)
+//           .where('status', isEqualTo: 'accepted')
+//           .get();
+
+//       List<Map<String, dynamic>> fetchedPatients = [];
+
+//       for (var doc in appointmentsSnapshot.docs) {
+//         Map<String, dynamic> patientData = doc.data() as Map<String, dynamic>;
+
+//         // Fetch profile picture URL from the 'patients' collection
+//         String patientId = patientData['patientId'];
+//         DocumentSnapshot patientSnapshot =
+//             await _firestore.collection('patients').doc(patientId).get();
+
+//         if (patientSnapshot.exists) {
+//           Map<String, dynamic>? patientInfo =
+//               patientSnapshot.data() as Map<String, dynamic>?;
+//           String profilePicUrl = patientInfo?['profile_image'] ?? '';
+
+//           // Add profilePicUrl to patientData
+//           patientData['profile_image'] = profilePicUrl;
+//           fetchedPatients.add(patientData);
+//         }
+//       }
+
+//       setState(() {
+//         patients = fetchedPatients;
+//         isLoading = false;
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         centerTitle: true,
+//         title: Text(
+//           'Chat with Patients',
+//           style: TextStyle(
+//               fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+//         ),
+//         flexibleSpace: Container(
+//           decoration: BoxDecoration(
+//             gradient: LinearGradient(
+//               colors: [Colors.blue, Colors.cyan],
+//               begin: Alignment.topLeft,
+//               end: Alignment.bottomRight,
+//             ),
+//           ),
+//         ),
+//       ),
+//       body: isLoading
+//           ? Center(child: CircularProgressIndicator())
+//           : Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Padding(
+//                   padding: const EdgeInsets.all(16.0),
+//                   child: Text(
+//                     'Patients',
+//                     style: TextStyle(
+//                       fontSize: 24,
+//                       fontWeight: FontWeight.bold,
+//                       color: Colors.blueGrey[800],
+//                     ),
+//                   ),
+//                 ),
+//                 Expanded(
+//                   child: patients.isEmpty
+//                       ? Center(child: Text('No patients to chat with'))
+//                       : ListView.builder(
+//                           padding: EdgeInsets.all(16),
+//                           itemCount: patients.length,
+//                           itemBuilder: (context, index) {
+//                             var patient = patients[index];
+//                             return Card(
+//                               elevation: 8,
+//                               shape: RoundedRectangleBorder(
+//                                 borderRadius: BorderRadius.circular(15),
+//                               ),
+//                               margin: EdgeInsets.only(bottom: 16),
+//                               child: Container(
+//                                 decoration: BoxDecoration(
+//                                   gradient: LinearGradient(
+//                                     colors: [Colors.white, Colors.blue[50]!],
+//                                     begin: Alignment.topLeft,
+//                                     end: Alignment.bottomRight,
+//                                   ),
+//                                 ),
+//                                 padding: const EdgeInsets.all(16.0),
+//                                 child: Row(
+//                                   children: [
+//                                     // Display profile picture or fallback to an icon if unavailable
+//                                     patient['profile_image'] != null &&
+//                                             patient['profile_image'] != ''
+//                                         ? ClipOval(
+//                                             child: Image.network(
+//                                               patient['profile_image'],
+//                                               width: 50,
+//                                               height: 50,
+//                                               fit: BoxFit.cover,
+//                                             ),
+//                                           )
+//                                         : Icon(Icons.person,
+//                                             color: Colors.blue),
+//                                     SizedBox(width: 8),
+//                                     Text(
+//                                       'Patient Name: ${patient['name']}',
+//                                       style: TextStyle(
+//                                         fontSize: 18,
+//                                         fontWeight: FontWeight.w600,
+//                                       ),
+//                                     ),
+//                                     Spacer(),
+//                                     IconButton(
+//                                       icon:
+//                                           Icon(Icons.chat, color: Colors.blue),
+//                                       onPressed: () {
+//                                         Navigator.push(
+//                                             context,
+//                                             MaterialPageRoute(
+//                                                 builder: (context) =>
+//                                                     DoctorChatScreen(
+//                                                       patientName:
+//                                                           patient['name'],
+//                                                       patientId:
+//                                                           patient['patientId'],
+//                                                     )));
+//                                       },
+//                                     )
+//                                   ],
+//                                 ),
+//                               ),
+//                             );
+//                           },
+//                         ),
+//                 ),
+//               ],
+//             ),
+//       bottomNavigationBar: DoctorNavigationBar(
+//         currentIndex: 3,
+//         onTap: (index) {
+//           Navigator.pop(context);
+//         },
+//       ),
+//     );
+//   }
+// }
--- lib/doctors_management/screens/chatscreen.dart
+++ lib/doctors_management/screens/chatscreen.dart
@@ -5,8 +5,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 class DoctorChatScreen extends StatefulWidget {
   final String patientName;
   final String patientId; // Receive the patientId
+  final String appointmentId;
 
-  DoctorChatScreen({required this.patientName, required this.patientId});
+  DoctorChatScreen(
+      {required this.patientName,
+      required this.patientId,
+      required this.appointmentId});
 
   @override
   _DoctorChatScreenState createState() => _DoctorChatScreenState();
@@ -33,7 +37,8 @@ class _DoctorChatScreenState extends State<DoctorChatScreen> {
     try {
       await _firestore.collection('chats').add({
         'doctorId': doctorId,
-        'patientId': widget.patientId, // Store patientId
+        'patientId': widget.patientId,
+        'appointmentId': widget.appointmentId,
         'message': _messageController.text,
         'timestamp': FieldValue.serverTimestamp(),
         'sender': 'doctor',
@@ -42,6 +47,63 @@ class _DoctorChatScreenState extends State<DoctorChatScreen> {
       _messageController.clear();
     } catch (e) {
       print('Error sending message: $e');
+    }
+  }
+
+  void _confirmDeleteMessage(String messageId, String sender) {
+    if (sender != 'doctor') {
+      // If the sender is not the doctor, do not allow deletion
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Cannot Delete Message'),
+            content: Text('You can only delete your own messages.'),
+            actions: [
+              TextButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      // If the sender is the doctor, allow deletion
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Delete Message'),
+            content: Text('Are you sure you want to delete this message?'),
+            actions: [
+              TextButton(
+                child: Text('Cancel'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: Text('Delete'),
+                onPressed: () {
+                  _deleteMessage(messageId);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  void _deleteMessage(String messageId) async {
+    try {
+      await _firestore.collection('chats').doc(messageId).delete();
+    } catch (e) {
+      print('Error deleting message: $e');
     }
   }
 
@@ -59,6 +121,7 @@ class _DoctorChatScreenState extends State<DoctorChatScreen> {
                   .collection('chats')
                   .where('doctorId', isEqualTo: doctorId)
                   .where('patientId', isEqualTo: widget.patientId)
+                  .where('appointmentId', isEqualTo: widget.appointmentId)
                   .orderBy('timestamp', descending: true)
                   .snapshots(),
               builder: (context, snapshot) {
@@ -84,36 +147,40 @@ class _DoctorChatScreenState extends State<DoctorChatScreen> {
                         ? (message['timestamp'] as Timestamp).toDate()
                         : null;
                     return ListTile(
-                      title: Align(
-                        alignment: message['sender'] == 'doctor'
-                            ? Alignment.centerRight
-                            : Alignment.centerLeft,
-                        child: Container(
-                          padding: const EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                            color: message['sender'] == 'doctor'
-                                ? Colors.blue
-                                : Colors.grey,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: message['sender'] == 'doctor'
-                                ? CrossAxisAlignment.end
-                                : CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                message['message'],
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              if (timestamp != null)
+                      title: GestureDetector(
+                        onLongPress: () => _confirmDeleteMessage(
+                            messages[index].id, message['sender']),
+                        child: Align(
+                          alignment: message['sender'] == 'doctor'
+                              ? Alignment.centerRight
+                              : Alignment.centerLeft,
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              color: message['sender'] == 'doctor'
+                                  ? Colors.blue
+                                  : Colors.grey,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: message['sender'] == 'doctor'
+                                  ? CrossAxisAlignment.end
+                                  : CrossAxisAlignment.start,
+                              children: [
                                 Text(
-                                  '${timestamp.hour}:${timestamp.minute}, ${timestamp.day}/${timestamp.month}/${timestamp.year}',
-                                  style: TextStyle(
-                                    color: Colors.white70,
-                                    fontSize: 10,
-                                  ),
+                                  message['message'],
+                                  style: TextStyle(color: Colors.white),
                                 ),
-                            ],
+                                if (timestamp != null)
+                                  Text(
+                                    '${timestamp.hour}:${timestamp.minute}, ${timestamp.day}/${timestamp.month}/${timestamp.year}',
+                                    style: TextStyle(
+                                      color: Colors.white70,
+                                      fontSize: 10,
+                                    ),
+                                  ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
